2c9d9dab3012334916b4028eb568c6287f2685d1
LinaLin <linalina0502@gmail.com>
2015-10-23 10:50:12 +1100

    reverted accidental changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,19 @@
 class User < ActiveRecord::Base
 	# Those following informations are required
  	validates_presence_of :email, :first_name, :last_name, :username, :password, :password_confirmation, :bio
- 	
+
  	# Set validates for these values
  	validates :first_name, :last_name, format: { with: /[A-Z]([a-z]+)/, message: "%{value} is not a valid name" }
   	validates :email, format: { with: /(.+)@(.+).[a-z]{2,4}/, message: "%{value} is not a valid email" }
   	validates :username, length: { minimum: 5 }
-  	validates :username, uniquen
+  	validates :username, uniqueness: true
+  	validates :password, :password_confirmation, length: { minimum: 8 }
+
+  	# Include this to store password safely
+	has_secure_password
+
+	# User can have tag list named interset list
+	acts_as_taggable_on :interests
 
 
 	# Find a user by username, then check the password is the same
@@ -24,12 +31,4 @@ class User < ActiveRecord::Base
 	def full_name
 		first_name + ' ' + last_name
 	end
-
-endess: true
-  	validates :password, :password_confirmation, length: { minimum: 8 }
-  	
-  	# Include this to store password safely
-	has_secure_password
-
-	# User can have tag list named interset list
-	acts_as_taggable_on :interests
+end
