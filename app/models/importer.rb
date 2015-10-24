b8e36ae87f3089608ba97bee580a07a7e1bd48d5
Hyeri Song <hyerisong@Hyeris-MacBook-Pro.local>
2015-10-25 10:59:33 +1100

    rearrange indentation

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -1,6 +1,6 @@
 # include modules required
 include Scrape
-#include Tag
+include Tag
 
 # importer class which import new articles using Scrape and Tag modules
 class Importer
@@ -30,20 +30,20 @@ class Importer
 			abc = Scrape::TheAbcScraper.new
 			@new_articles << abc.scrape
 
-			#sbs = Scrape::TheSbsScraper.new
-	    #	@new_articles << sbs.scrape
+			sbs = Scrape::TheSbsScraper.new
+   		@new_articles << sbs.scrape
 
-	    #	guardian = Scrape::TheGuardianScraper.new
-	    #	@new_articles << guardian.scrape
+   		guardian = Scrape::TheGuardianScraper.new
+   		@new_articles << guardian.scrape
 
-	    #	sydney = Scrape::TheSydneyMorningHeraldScraper.new
-	    #	@new_articles << sydney.scrape
+   		sydney = Scrape::TheSydneyMorningHeraldScraper.new
+   		@new_articles << sydney.scrape
 
-	    new_york = Scrape::TheNewYorkTimesScraper.new
-	    @new_articles << new_york.scrape
+   		new_york = Scrape::TheNewYorkTimesScraper.new
+   		@new_articles << new_york.scrape
 
-			#age = Scrape::TheAgeScraper.new
-			#@new_articles << age.scrape
+			age = Scrape::TheAgeScraper.new
+			@new_articles << age.scrape
 		end
 
     @new_articles = @new_articles.flatten
@@ -53,44 +53,42 @@ class Importer
 
 	# tag all new articles
 	def tag_all
-		threads = []
-		ActiveRecord::Base.transaction do
-			@new_articles.each do |a|
-				begin
-					@tsource.tag_by_source(a)
-				rescue
-					puts "Tagging article by source failed"
-				end
-
-				begin
-					@tauthor.tag_by_author(a)
-				rescue
-					puts "Tagging article by author failed"
-				end
-
-				begin
-					@tcategory.tag_by_category(a)
-				rescue
-					puts "Tagging article by category failed"
-				end
-
-				begin
-						@tsummary.tag_by_summary(a)
-				rescue
-						puts "Tagging article by summary failed"
-				end
-
-				begin
-						@ttitle.tag_by_title(a)
-				rescue
-						puts "Tagging article by title failed"
-				end
-				
-				a.tag_list = a.tag_list.uniq
-				a.save
+		# tag article one by one
+		@new_articles.each do |a|
+			begin
+				@tsource.tag_by_source(a)
+			rescue
+				puts "Tagging article by source failed"
 			end
 
+			begin
+				@tauthor.tag_by_author(a)
+			rescue
+				puts "Tagging article by author failed"
+			end
+
+			begin
+				@tcategory.tag_by_category(a)
+			rescue
+				puts "Tagging article by category failed"
+			end
+
+			begin
+				@tsummary.tag_by_summary(a)
+			rescue
+				puts "Tagging article by summary failed"
+			end
+
+			begin
+				@ttitle.tag_by_title(a)
+			rescue
+				puts "Tagging article by title failed"
+			end
+
+			a.tag_list = a.tag_list.uniq
+			a.save
 		end
+
 	end
 
 end
